1c902629b101646f6f96c4cd8ef48ec5216b9389
arthurpansera <arthurpansera0308@gmail.com>
2024-04-17 23:13:17 -0300

    Projeto Pokémon
    Projeto RA1: Desafio Pokémon (Parte 1)

--- jogopokemon.py
+++ jogopokemon.py
@@ -30,19 +30,18 @@ else:
 
 print(70*"-")
 
+print("\nO que você deseja fazer?\n1. Entrar na caverna\n2. Entrar no mato\n3. Listar Pokémon na Pokédex\n4. Sair")
+escolha = int(input("Escolha uma opção: "))
+
+while escolha != 1 and escolha != 2 and escolha != 3 and escolha != 4:
+    escolha = int(input("Opção inválida! Escolha uma opção: "))
+
 import random
 pokemonsCaverna = ["Zubat","Geodude","Paras"]
 pokemonsMato = ["Caterpie","Weedle","Pidgey","Rattata"]
 probCaverna = 0.35
 probMato = 0.5
 
-while escolha != 4:
-    print("\nO que você deseja fazer?\n1. Entrar na caverna\n2. Entrar no mato\n3. Listar Pokémon na Pokédex\n4. Sair")
-    escolha = int(input("Escolha uma opção: "))
-    
-while escolha != 1 and escolha != 2 and escolha != 3 and escolha != 4:
-        escolha = int(input("Opção inválida! Escolha uma opção: "))
-    
 if escolha == 1:
     pokemon = random.choice(pokemonsCaverna)
     print(f"Você entrou na caverna e encontrou um {pokemon}")
