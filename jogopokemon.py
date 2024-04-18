27ee36f6091e51ce3c3e3b55322b07552e342f8d
arthurpansera <arthurpansera0308@gmail.com>
2024-04-17 21:26:59 -0300

    Projeto Pokémon
    Projeto RA1: Desafio Pokémon (Parte 1)

--- jogopokemon.py
+++ jogopokemon.py
@@ -9,6 +9,32 @@ print("Esse mundo é habitado por várias criaturas chamadas de Pokémon.")
 print("Algumas pessoas os tratam como mascotes, outras usam em batalhas.")
 nome = input("Antes de começar sua jornada, qual é o seu nome?\nInforme seu nome: ")
 print(f"Ótimo! Prazer em conhecê-lo, {nome}!\nPrepare-se para embarcar em uma aventura emocionante!\n")
+print("Primeiro você deve escolher o seu Pokémon inicial. Há três opções: ")
+
+pokemon_inicial = int(input("1. Bulbasaur\n2. Squirtle\n3. Charmander\nFaça sua escolha: "))
+pokemons_iniciais = ["Bulbasaur","Squirtle","Charmander"]
+pokedex = []
+
+if pokemon_inicial == 1:
+    escolha = int(input("Então você quer ficar com Bulbasaur do tipo grama? (1- Sim / 2- Não)"))
+    print("Ótima escolha!")
+    print("*Bulbasaur foi adicionado a sua Pokédex")
+    pokedex.append("Bulbasaur")
+    print(pokedex)
+elif pokemon_inicial == 2:
+    escolha = int(input("Então você quer ficar com Squirtle do tipo água? (1- Sim / 2- Não)"))
+    print("Ótima escolha!")
+    print("*Squirtle foi adicionado a sua Pokédex")
+    pokedex.append("Squirtle")
+    print(pokedex)
+elif pokemon_inicial == 3:
+    escolha = int(input("Então você quer ficar com Charmander do tipo fogo? (1- Sim / 2- Não)"))
+    print("Ótima escolha!")
+    print("*Charmander foi adicionado a sua Pokédex")
+    pokedex.append("Charmander")
+    print(pokedex)
+else:
+    pokemon_inicial = int(input("Opção inválida. Faça sua escolha: "))
 
 print(70*"-")
 
@@ -17,7 +43,6 @@ print("1. Entrar na caverna\n2. Entrar no mato\n3. Lista Pokémon na Pokédex\n4
 escolha = int(input("Escolha uma opção: "))
 
 import random
-pokedex = []
 pokemonsCaverna = ["Zubat","Geodude","Paras"]
 pokemonsMato = ["Caterpie","Weedle","Pidgey","Rattata"]
 if escolha == 1:
