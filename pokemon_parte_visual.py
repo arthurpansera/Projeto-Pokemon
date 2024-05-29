a3cf0c25809b5f800581e61a7fba21db0f59138c
arthurpansera <163167633+arthurpansera@users.noreply.github.com>
2024-05-29 11:34:20 -0300

    Pokémon
    Projeto Pokémon

--- pokemon_parte_visual.py
+++ pokemon_parte_visual.py
@@ -1,13 +1,24 @@
 #from tkinter import
 import tkinter as tk
+from PIL import Image, ImageTk
 
 pokedex = [
-    ["Pikachu","Electric",35,55,40],
-    ["Bulbasaur","Grass",45,49,60],
-    ["Charmander","Fire",39,52,42]
+    ["Pikachu","imagens/pikachu.png","Electric",35,55,40],
+    ["Bulbasaur","imagens/bulbasaur.png","Grass",45,49,60],
+    ["Charmander","imagens/charmander.png","Fire",39,52,42]
 ]
 
-#Função 
+#Função para atualizar a exibição do Pokémon(Imagem e dados)
+def update_pokemon(event):
+    pokemon_selecionado = listbox_pokemon.curselection()
+    if pokemon_selecionado:
+        #A posição 0 nos dá o índice da listbox
+        index = pokemon_selecionado[0]
+        pokemon = pokedex[index]
+        #Pegar a imagem e padronizar
+        img_pokemon = Image.open(pokemon[1])
+        img_pokemon = img_pokemon.resize([200,200], Image.ANTIALIAS)
+        img_pokemon = ImageTk.PhotoImage(img_pokemon)
 
 #Inicializando a janela
 janela = tk.Tk()
@@ -27,6 +38,8 @@ listbox_pokemon = tk.Listbox(frame_left)
 for pokemon in pokedex:
     listbox_pokemon.insert(0,pokemon[0])
 listbox_pokemon.pack()
+
+listbox_pokemon.bind('<<ListBoxSelect>>', update_pokemon)
 
 btn_exit = tk.Button(frame_left,text="Sair",command=janela.quit)
 btn_exit.pack()
