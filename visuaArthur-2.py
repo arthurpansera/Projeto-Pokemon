c84778533b379c3b63ce776e145f9df9f0221173
arthurpansera <arthurpansera0308@gmail.com>
2024-06-16 13:06:43 -0300

    Pokémon
    Projeto Pokémon

--- visuaArthur-2.py
+++ visuaArthur-2.py
@@ -268,10 +268,139 @@ imagem_squirtle.place(x=345, y=230)
 imagem_charmander = Label(frame_pokebolas, bg="#88c088")
 imagem_charmander.place(x=540, y=230)
 
-btn_avancar = tk.Button(frame_pokebolas, command=lambda:iniciar_menu(btn_avancar), image=image_proximo, width=0, height=0, relief="raised", anchor=NW, padx=1, pady=1, bg='#d8e3e3')
-btn_avancar.place(x=655, y=390)
+frame_menu = Frame(janela, width=1200, height=1000, bg="#3A8C73")
+frame_menu.pack()
 
-pokedex = []
+frame_pokedex = Frame(janela, width=1200, height=1000, bg ="#d93035")
+frame_pokedex.pack()
+
+pokedex = [
+    #Name, imagem, Primary Type,Secondary type,Attack,Defense,HP,Sp.Attack,Sp.Defense,Speed,Total
+    ['Bulbasaur','imagens/bulbasaur.png', 'GRASS,POISON','',49,49,45,65,65,45,318,],
+    ['Charmander','imagens/charmander.png','FIRE','',52,43,39,60,50,65,309,],
+    ['Squirtle','imagens/squirtle.png','WATER','',48,65,44,50,64,43,314,],
+    ['Caterpie','imagens/caterpie.png','BUG','',30,35,45,20,20,45,195, ],
+    ['Weedle','imagens/weedle.png','BUG','POISON',35,30,40,20,20,50,195, ],
+    ['Pidgey','imagens/pidgey.png','NORMAL','FLYING',45,40,40,35,35,56,251,],
+    ['Spearow', 'imagens/spearow.png','NORMAL','FLYING',60,30,40,31,31,70,262,],
+    ['Rattata','imagens/rattata.png', 'NORMAL','',56,35,30,25,35,72,253,],
+    ['Ekans','imagens/ekans.png','POISON','',60,44,35,40,54,55,288,],
+    ['Pikachu','imagens/pikachu.png','ELECTRIC','',55,40,35,50,50,90,320,]
+]
+
+borderImage = Image.open("imagens/border.png")
+borderImage = borderImage.resize((600, 530))
+borderImage = ImageTk.PhotoImage(borderImage)
+lbl_borderImage = Label(frame_pokedex, image=borderImage, bg="#d93035")
+lbl_borderImage.place(x=400, y=20)
+
+infoImage = Image.open("imagens/info.png")
+infoImage = infoImage.resize((750, 250))
+infoImage = ImageTk.PhotoImage(infoImage)
+lbl_infoImage = Label(frame_pokedex, image=infoImage, bg="#d93035")
+lbl_infoImage.place(x=330, y=580)
+
+lbl_name = Label(frame_pokedex, text="", relief="flat", anchor=CENTER, font=("Fixedsys 30 bold"), fg="black", bg="#e3e3e3")
+lbl_name.place(x=780, y=485)
+
+lbl_image = Label(frame_pokedex, bg="#598f60")
+lbl_image.place(x=473, y=90)
+
+#Habilidade do Pokemons
+lbl_habilities = Label(frame_pokedex, text="Habilidade", relief="flat", anchor=CENTER, font=("Fixedsys 23"), fg="black", bg="#8dc73f")
+lbl_habilities.place(x=790, y=610)
+
+lbl_type = Label(frame_pokedex, text="", relief="flat", anchor=CENTER, font=("Fixedsys 17"), fg="black", bg="#8dc73f")
+lbl_type.place(x=790, y=650)
+
+lbl_secondtype = Label(frame_pokedex, text="", relief="flat", anchor=CENTER, font=("Fixedsys 17"), fg="black", bg="#8dc73f")
+lbl_secondtype.place(x=790, y=680)
+
+#Informações do Pokemon
+lbl_status = Label(frame_pokedex, text="Status", relief="flat", anchor=CENTER, font=("Fixedsys 23"), fg="black", bg="#8dc73f")
+lbl_status.place(x=400, y=610)
+
+lbl_hp = Label(frame_pokedex, text="", relief="flat", anchor=CENTER, font=("Fixedsys 15"), fg="black", bg="#8dc73f")
+lbl_hp.place(x=400, y=650)
+
+lbl_attack = Label(frame_pokedex, text="", relief="flat", anchor=CENTER, font=("Fixedsys 15"), fg="black", bg="#8dc73f")
+lbl_attack.place(x=400, y=680)
+
+lbl_defense = Label(frame_pokedex, text="", relief="flat", anchor=CENTER, font=("Fixedsys 15"), fg="black", bg="#8dc73f")
+lbl_defense.place(x=400, y=710)
+
+lbl_spAttack = Label(frame_pokedex, text="", relief="flat", anchor=CENTER, font=("Fixedsys 15"), fg="black", bg="#8dc73f")
+lbl_spAttack.place(x=400, y=740)
+
+lbl_spDefense = Label(frame_pokedex, text="", relief="flat", anchor=CENTER, font=("Fixedsys 15"), fg="black", bg="#8dc73f")
+lbl_spDefense.place(x=400, y=770)
+
+lbl_speed = Label(frame_pokedex, text="", relief="flat", anchor=CENTER, font=("Fixedsys 15"), fg="black", bg="#8dc73f")
+lbl_speed.place(x=540, y=650)
+
+lbl_total = Label(frame_pokedex, text="", relief="flat", anchor=CENTER, font=("Fixedsys 15"), fg="black", bg="#8dc73f")
+lbl_total.place(x=540, y=680)
+
+#Botoes para Cada Pokemon
+imageBulbasaurIcone = Image.open("imagens/icone-bulbasaur.png")
+imageBulbasaurIcone = imageBulbasaurIcone.resize((60, 63))
+imageBulbasaurIcone = ImageTk.PhotoImage(imageBulbasaurIcone)
+btn_Bulbasaur = Button(frame_pokedex, command=lambda: atualizar_informacoes_pokedex(nome="Bulbasaur"), image=imageBulbasaurIcone, text=(f"Bulbasaur "), width=160, height=55, relief="raised", overrelief=RIDGE, compound=RIGHT, anchor=NW, padx=10, font=("Fixedsys 15"), bg='white', fg='black')
+btn_Bulbasaur.place(x=10, y=100)
+
+imageCharmanderIcone = Image.open("imagens/icone-charmander.png")
+imageCharmanderIcone = imageCharmanderIcone.resize((60, 63))
+imageCharmanderIcone = ImageTk.PhotoImage(imageCharmanderIcone)
+btn_Charmander = Button(frame_pokedex,command=lambda: atualizar_informacoes_charmander(btn_Charmander), image=imageCharmanderIcone, text=(f"Charmander"), width=160, height=55, relief="raised", overrelief=RIDGE, compound=RIGHT, anchor=NW, padx=10, font=("Fixedsys 15"), bg='white', fg='black')
+btn_Charmander.place(x=10, y=160)
+
+imageSquirtleIcone = Image.open("imagens/icone-squirtle.png")
+imageSquirtleIcone = imageSquirtleIcone.resize((60, 63))
+imageSquirtleIcone = ImageTk.PhotoImage(imageSquirtleIcone)
+btn_Squirtle= Button(frame_pokedex,command=lambda: atualizar_informacoes_squirtle(btn_Squirtle), image=imageSquirtleIcone, text=(f"Squirtle  "), width=160, height=55, relief="raised", overrelief=RIDGE, compound=RIGHT, anchor=NW, padx=10, font=("Fixedsys 15"), bg='white', fg='black')
+btn_Squirtle.place(x=10, y=220)
+
+imageCaterpieIcone = Image.open("imagens/icone-caterpie.png")
+imageCaterpieIcone = imageCaterpieIcone.resize((60, 63))
+imageCaterpieIcone = ImageTk.PhotoImage(imageCaterpieIcone)
+btn_Caterpie= Button(frame_pokedex,command=lambda: atualizar_informacoes_caterpie(btn_Caterpie), image=imageCaterpieIcone, text=(f"Caterpie  "), width=160, height=55, relief="raised", overrelief=RIDGE, compound=RIGHT, anchor=NW, padx=10, font=("Fixedsys 15"), bg='white', fg='black')
+btn_Caterpie.place(x=10, y=280)
+
+imageWeedleIcone = Image.open("imagens/icone-weedle.png")
+imageWeedleIcone = imageWeedleIcone.resize((60, 63))
+imageWeedleIcone = ImageTk.PhotoImage(imageWeedleIcone)
+btn_Weedle= Button(frame_pokedex, command=lambda: atualizar_informacoes_weedle(btn_Weedle), image=imageWeedleIcone, text=(f"Weedle    "), width=160, height=55, relief="raised", overrelief=RIDGE, compound=RIGHT, anchor=NW, padx=10, font=("Fixedsys 15"), bg='white', fg='black')
+btn_Weedle.place(x=10, y=340)
+
+imagePidgeyIcone = Image.open("imagens/icone-pidgey.png")
+imagePidgeyIcone = imagePidgeyIcone.resize((60, 63))
+imagePidgeyIcone = ImageTk.PhotoImage(imagePidgeyIcone)
+btn_Pidgey= Button(frame_pokedex, command=lambda: atualizar_informacoes_pidgey(btn_Pidgey), image=imagePidgeyIcone, text=(f"Pidgey    "), width=160, height=55, relief="raised", overrelief=RIDGE, compound=RIGHT, anchor=NW, padx=10, font=("Fixedsys 15"), bg='white', fg='black')
+btn_Pidgey.place(x=10, y=400)
+
+imageSpearowIcone = Image.open("imagens/icone-spearow.png")
+imageSpearowIcone = imageSpearowIcone.resize((60, 63))
+imageSpearowIcone = ImageTk.PhotoImage(imageSpearowIcone)
+btn_Spearow= Button(frame_pokedex, command=lambda: atualizar_informacoes_spearow(btn_Spearow), image=imageSpearowIcone, text=(f"Spearow   "), width=160, height=55, relief="raised", overrelief=RIDGE, compound=RIGHT, anchor=NW, padx=10, font=("Fixedsys 15"), bg='white', fg='black')
+btn_Spearow.place(x=10, y=460)
+
+imageRattataIcone = Image.open("imagens/icone-rattata.png")
+imageRattataIcone = imageRattataIcone.resize((60, 63))
+imageRattataIcone = ImageTk.PhotoImage(imageRattataIcone)
+btn_Rattata= Button(frame_pokedex,command=lambda: atualizar_informacoes_rattata(btn_Rattata), image=imageRattataIcone, text=(f"Rattata   "), width=160, height=55, relief="raised", overrelief=RIDGE, compound=RIGHT, anchor=NW, padx=10, font=("Fixedsys 15"), bg='white', fg='black')
+btn_Rattata.place(x=10, y=520)
+
+imageEkansIcone = Image.open("imagens/icone-ekans.png")
+imageEkansIcone = imageEkansIcone.resize((60, 63))
+imageEkansIcone = ImageTk.PhotoImage(imageEkansIcone)
+btn_Ekans = Button(frame_pokedex, command=lambda: atualizar_informacoes_ekans(btn_Ekans), image=imageEkansIcone, text=(f"Ekans     "), width=160, height=55, relief="raised", overrelief=RIDGE, compound=RIGHT, anchor=NW, padx=10, font=("Fixedsys 15"), bg='white', fg='black')
+btn_Ekans.place(x=10, y=580)
+
+imagePikachuIcone = Image.open("imagens/icone-pikachu.png")
+imagePikachuIcone = imagePikachuIcone.resize((60, 63))
+imagePikachuIcone = ImageTk.PhotoImage(imagePikachuIcone)
+btn_Pikachu = Button(frame_pokedex, command=lambda: atualizar_informacoes_pikachu(btn_Pikachu), image=imagePikachuIcone, text=(f"Pikachu   "), width=160, height=55, relief="raised", overrelief=RIDGE, compound=RIGHT, anchor=NW, padx=10, font=("Fixedsys 15"), bg='white', fg='black')
+btn_Pikachu.place(x=10, y=640)
 
 iniciar_jogo()
 janela.mainloop()
